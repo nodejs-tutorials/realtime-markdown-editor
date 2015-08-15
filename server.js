6739b6f21c60180667b4e1bcbed047b44d120f04
Nguyen Ngoc Duc <nnduc@smartdev.vn>
2015-08-15 23:49:38 +0700

    fixed

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 var express = require('express');
-var app     = express();
+var app = express();
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-app.set('view engine','ejs');
+// public folder to store assets
+app.use(express.static(__dirname + '/public'));
 
-app.use(express.static(__dirname+'/public'));
-
-app.get('/', function (req,res) {
+// routes for app
+app.get('/', function(req, res) {
     res.render('pad');
-
+});
+app.get('/(:id)', function(req, res) {
+    res.render('pad');
 });
 
+// get sharejs dependencies
+var sharejs = require('share');
+require('redis');
+
+// options for sharejs 
+var options = {
+    db: {type: 'redis'}
+};
+
+// attach the express server to sharejs
+sharejs.server.attach(app, options);
+
+// listen on port 8000 (for localhost) or the port defined for heroku
 var port = process.env.PORT || 8000;
 app.listen(port);
-
-console.log('Server is started: http://localhost:8000');
